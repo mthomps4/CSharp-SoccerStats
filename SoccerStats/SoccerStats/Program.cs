3fde7b28f5308122fc70b93a414e806856d4e3c9
Matthew Thompson <matthew.thompson.a@gmail.com>
2016-09-23 13:42:26 -0400

    Working with File Directory and StreamReader

--- SoccerStats/SoccerStats/Program.cs
+++ SoccerStats/SoccerStats/Program.cs
@@ -11,7 +11,33 @@ namespace SoccerStats
     {
         static void Main(string[] args)
         {
-            string currentDirectory = Directory.GetCurrentDirectory(); 
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+
+            var fileName = Path.Combine(directory.FullName, "data.txt"); //Path.Combine will add / if / missing 
+            var file = new FileInfo(fileName);
+
+            if (file.Exists)
+            {
+                //var reader = new streamreader(file.fullname);
+                //try
+                //{
+                //    console.setin(reader);
+                //    console.writeline(console.readline());
+                //}
+                //finally
+                //{
+                //    reader.close();
+                //}
+
+                //simple way 
+                using (var reader = new StreamReader(file.FullName))
+                {
+                    Console.SetIn(reader);
+                    Console.WriteLine(Console.ReadLine());  
+                }//calls dispose method which calls the close method 
+            }
+
         }
     }
 }

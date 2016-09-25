fef5f895c30846ada4c5f85f0568764fd6844c4e
Matt Thompson <matthew.thompson.a@gmail.com>
2016-09-25 15:30:09 -0400

    Created SerializePlayerToFile "topTen.json"

---
+++ SoccerStats/SoccerStats/PlayerComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerStats
+{
+    class PlayerComparer : IComparer<Player>
+    {
+        public int Compare(Player x, Player y)
+        {
+            return x.PointsPerGame.CompareTo(y.PointsPerGame) * -1; //returns opposite of accending 
+        }
+    }
+}
